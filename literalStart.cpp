410038eb163e5fb53378d3e4ae59da74412b97ac
Amukh1 <85003861+amukh1@users.noreply.github.com>
2023-03-28 21:42:33 -0400

    C++...

--- literalStart.cpp
+++ literalStart.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+
  
 using namespace std;
+
+// vector<string> split(const string& str, const string& delim)
+// {
+//     vector<string> tokens;
+//     size_t prev = 0, pos = 0;
+//     do
+//     {
+//         pos = str.find(delim, prev);
+//         if (pos == string::npos) pos = str.length();
+//         string token = str.substr(prev, pos-prev);
+//         if (!token.empty()) tokens.push_back(token);
+//         prev = pos + delim.length();
+//     }
+//     while (pos < str.length() && prev < str.length());
+//     return tokens;
+// }
+
+class CPU {
+  public:
+  void runLine(string line) {
+    // vector<string> tokens = split(line, " ");
+cout << "C++..." << endl;
+  };
+
+};
  
 // This program prints out “Hello World!”
 int main(int argc, char* argv[]) {
@@ -10,10 +36,20 @@ int main(int argc, char* argv[]) {
   cout << message;
   int a = 4;
   int* b = &a;
-  cout << b << endl;
-  cout << *b << endl;
-  cout << &a << endl;
-  cout << bitset<8>(2).to_string() << endl;
+  // cout << b << endl;
+  // cout << *b << endl;
+  // cout << &a << endl;
+  string binary = bitset<8>(a).to_string(); //to binary
+  cout<<binary<<"\n";
+
+  unsigned long decimal = bitset<8>(binary).to_ulong();
+  cout<<decimal<<"\n";
+
+  string line = "0001 hi";
+
+  CPU cpu;
+
+  cpu.runLine(line);
     
   return 0;
 }
